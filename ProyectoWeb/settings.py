dc47247c0d36a180a3a15d1d63bdb43e75cc21c3
Antonio Porcar <ad.porcar@gmail.com>
2024-11-29 16:43:34 +0100

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=_m9c(-c=)(-yb(920dd2w01^zft8h(k&dye#j#@0viv^_8z_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #para cargar imagenes
 MEDIA_URL = '/media/'
